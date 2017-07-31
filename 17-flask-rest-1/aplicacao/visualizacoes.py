4b0d2c93632e2fae50593e45eb2f2ff8d33b9233
Miguel Suez Xve Penteado <sp-miguel@sp100333932.ibge.gov.br>
2017-07-31 01:13:09 -0300

    31-07-2017

--- 17-flask-rest-1/aplicacao/visualizacoes.py
+++ 17-flask-rest-1/aplicacao/visualizacoes.py
@@ -53,3 +53,28 @@ def inserir_produto():
     '''
     curl -i -H "Content-Type: application/json" -X POST -d '{"titulo":"Livros"}' http://localhost:5000/todo/api/v1.0/produtos
     '''
+@variavel_aplicacao.route('/todo/api/v1.0/produtos/<int:codigo_produto>', methods=['PUT'])
+def atualiza_produto(codigo_produto):
+    produto = [produto for produto in tabela_produtos if produto['id'] == codigo_produto]
+    if len(produto) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'titulo' in request.json and type(request.json['titulo']) != unicode:
+        abort(400)
+    if 'descricao' in request.json and type(request.json['descricao']) is not unicode:
+        abort(400)
+    if 'disponivel' in request.json and type(request.json['disponivel']) is not bool:
+        abort(400)
+    produto[0]['titulo'] = request.json.get('titulo', produto[0]['titulo'])
+    produto[0]['descricao'] = request.json.get('descricao', produto[0]['descricao'])
+    produto[0]['disponivel'] = request.json.get('disponivel', produto[0]['disponivel'])
+    return jsonify({'produto': produto[0]})
+
+@variavel_aplicacao.route('/todo/api/v1.0/produtos/<int:codigo_produto>', methods=['DELETE'])
+def apaga_produto(parametro_codigo_produto):
+    produto = [produto for produto in tabela_produtos if produto['id'] == parametro_codigo_produto]
+    if len(produto) == 0:
+        abort(404)
+    tabela_produtos.remove(produto[0])
+    return jsonify({'result': True})
